235cacfb96231c580b2a5491855480c18c1cd97a
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-28 21:19:44 +0100

    imported the Navbar and home component from to the App component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem10', author: 'mario', id: 1},
+        {title: 'Welcome party', body: 'lorem20', author: 'yoshi', id: 2},
+        {title: 'web dev top tips', body: 'lorem30', author: 'mario', id: 3}
+    ]);
+    
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
