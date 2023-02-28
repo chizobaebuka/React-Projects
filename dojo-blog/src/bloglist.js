fec4a74477a907285c51ee5c2386fb2a79f0d3e3
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-28 21:51:47 +0100

    called the handleDelete function created in the Home.js component to the bloglist component and also then used the same function to delete a certain blog as you require which is passed as a prop

--- dojo-blog/src/bloglist.js
+++ dojo-blog/src/bloglist.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
     // const title = props.title;
     return (
@@ -7,7 +7,8 @@ const BlogList = ({ blogs, title }) => {
              {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
-                    <p>{ blog.author }</p>
+                    <p> written by { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
