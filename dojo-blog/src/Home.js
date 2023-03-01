1458357069028965b78c58599c08fb958e39922b
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-01 12:08:29 +0100

    using the useEffect and fetching endpoints to get the info of the blogs from the db.json server file

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './bloglist';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem10', author: 'mario', id: 1},
-        {title: 'Welcome party', body: 'lorem20', author: 'yoshi', id: 2},
-        {title: 'web dev top tips', body: 'lorem30', author: 'mario', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+     
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-    
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsLoading(false)
+            })
+        }, 1000);
+    }, []);
+
     return (
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            { isLoading && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
     );
 }
