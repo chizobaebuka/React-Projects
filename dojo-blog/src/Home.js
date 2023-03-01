8bf8688e52cd7e0d71aad13e9b8a38b0f69e3cb4
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-01 23:33:30 +0100

    modified

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,28 +1,15 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import BlogList from './bloglist';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true)
-     
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsLoading(false)
-            })
-        }, 1000);
-    }, []);
-
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs')
+ 
     return (
         <div className="home">
+            { error && <div>{ error } </div> }
             { isLoading && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
     );
 }
