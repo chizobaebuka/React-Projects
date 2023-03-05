e9b0d5f6d753610448c9599a0f906fe4a909cf99
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-06 00:12:59 +0100

    initializing the project

--- city-guide/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// Mounting the application to the root div
-ReactDOM.render(<App />, document.getElementById('root'));
