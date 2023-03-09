0dd594e61476286086420362b7205e212ef610e8
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-09 16:02:51 +0100

    added pagination to the website ensuring not more than 9 items are shown upon whatever is seardched for

--- stay-fit/src/components/Exercises.js
+++ stay-fit/src/components/Exercises.js
@@ -1,25 +1,52 @@
 import React, { useEffect, useState} from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
+
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises)
-  
+  const [ currentPage, setCurrentPage ] = useState(1)
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: 'smooth' })
+  }
+
+
   return (
     <Box id="exercises"
       sx={{mt: { lg: '110px' }}}
       mt="50px"
       p="20px"
     >
-      <Typography variant="h3" mb="46px">
+      <Typography variant="h4" mb="46px">
         Showing Results
       </Typography>
-      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyCOntent="center">
-        {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise}/>
         ))}
+      </Stack>
+      <Stack  alignItems="center" mt="100px">
+        {exercises.length > 9 && (
+          <Pagination 
+            color="standard" 
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   )
