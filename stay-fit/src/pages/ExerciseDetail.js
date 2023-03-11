2f2760a6fd13df6a4e77a1ac324154e33c84fcb5
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-11 01:42:00 +0100

    added a function to fetch both the target Muscle exercises and the equipment Exercises

--- stay-fit/src/pages/ExerciseDetail.js
+++ stay-fit/src/pages/ExerciseDetail.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
+
+
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
   const { id } = useParams();
+  
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -18,7 +24,17 @@ const ExerciseDetail = () => {
       // calling from the api
       const exerciseDetailData = await fetchData (`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
-    }
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideosData.contents)
+    
+      const targetMuscleExercisesData = await fetchData( `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData( `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData);
+    };
+
     fetchExercisesData();
   }, [id])
   
@@ -26,8 +42,9 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos />
-      <SimilarExercises />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises}
+      equipmentExercises={equipmentExercises}/>
     </Box>
   )
 }
