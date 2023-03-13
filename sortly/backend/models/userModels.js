85aca3e44aacc617d2a8f4ca91504a62f9db2106
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-13 17:43:28 +0100

    created the user registration model

---
+++ sortly/backend/models/userModels.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true, 
+        trim: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: [6, "Password must be up to 6 characters"],
+        maxLength: [23, "Password must not be more than 23 characters"],
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default: "https://i.ibb.co/4pDNDk1/avatar.png"
+    },
+    phone: {
+        type: String,
+        required: [true, "Please add a number"],
+        default: "+234"
+    },
+    bio: {
+        type: String,
+        required: [true, "Bio must not be more than 250 characters"],
+        default: "bio"
+    }, 
+    },
+    {
+        timeStamps: true,
+    }
+);
+
+const USer = mongoose.model("User", userSchema);
+module.exports = User
