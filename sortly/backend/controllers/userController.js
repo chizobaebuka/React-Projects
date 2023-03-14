98faf824242018b48e0fd4d3a14a6d253420a4c1
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-15 00:16:04 +0100

    logging in the user into the site done

--- sortly/backend/controllers/userController.js
+++ sortly/backend/controllers/userController.js
@@ -1,14 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModels");
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
-}
+};
 
 
-
+// Registers user
 const registerUser = asyncHandler( async (req, res) => {
     const {name, email, password} = req.body
     // ensuring the user didn't omit some requirments
@@ -36,19 +36,74 @@ const registerUser = asyncHandler( async (req, res) => {
     })
 
     // Generate tOken
-    const token = generateToken(user._id)
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400),
+        sameSite: "none", 
+        secure: true
+    });
 
     if(user) {
         const { _id, name, email, photo, phone, bio} = user
         res.status(201).json({
             _id, name, email, photo, phone, bio, token,
-        })
+        });
     } else{ 
         res.status(400)
         throw new Error ("Invalid user data")
     }
 });
+
+// Login the user
+const loginUser = asyncHandler(async (req, res) => {
+    
+    const { email, password } = req.body
+    // Validate request
+    if (!email || !password){
+        res.status(400);
+        throw new Error ("Please add email and password");
+    }
+
+    // checking if the user exists in the DB
+    const user = await User.findOne({email})
+    if (!user){
+        res.status(400);
+        throw new Error ("User not found please sign up ");
+    }
+
+    // User exists, check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+    // Generate token using the user id
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400),
+        sameSite: "none", 
+        secure: true
+    });
+
+    if(user && passwordIsCorrect){
+        const { _id, name, email, photo, phone, bio} = user
+        res.status(200).json({
+            _id, name, email, photo, phone, bio, token,
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
+
+});
     
 module.exports = {
     registerUser,
+    loginUser,
 };
