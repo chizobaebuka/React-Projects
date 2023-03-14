8906b6d5b0872ff53440181923cb7ddd691720b0
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-15 00:34:09 +0100

    added the getUser route to this file

--- sortly/backend/routes/userRoute.js
+++ sortly/backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController")
+const { registerUser, loginUser, logout } = require("../controllers/userController")
 
 // const registerUser = () => {};
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getUser", getUser);
 
 module.exports = router;
