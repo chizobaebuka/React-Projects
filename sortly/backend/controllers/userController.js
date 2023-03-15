0f5e4fd45802e30984fd64b148b9203898e4303a
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-15 01:19:28 +0100

    get user data added

--- sortly/backend/controllers/userController.js
+++ sortly/backend/controllers/userController.js
@@ -102,8 +102,41 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 
 });
+
+// Logout user
+const logout = asyncHandler (async (req, res) => {
+    // Send HTTP-only cookie
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: "none", 
+        secure: true
+    });
+    return res.status(200).json({ message: 'Successfully Logged Out ' });
+});
+
+// Get User Data
+const getUser = asyncHandler (async (req, res) => {
+    const user = user.findById(req.user._id);
+    if(user){
+        if(user){
+            const { _id, name, email, photo, phone, bio} = user
+            res.status(200).json({
+                _id, name, email, photo, phone, bio, 
+            });
+        }
+        else {
+            res.status(400);
+            throw new Error("User Not Found");
+        }
+    
+    }
+})
     
 module.exports = {
     registerUser,
     loginUser,
+    logout,
+    getUser,
 };
