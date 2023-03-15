2e5015f893f52c58616ef5f7022cb40deda4ee37
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-15 11:59:11 +0100

    getuser log in status

--- sortly/backend/routes/userRoute.js
+++ sortly/backend/routes/userRoute.js
@@ -5,15 +5,14 @@ const {
     loginUser, 
     logout, 
     getUser,
+    loginStatus
 } = require("../controllers/userController");
-const protect = require("..//middleWare/authMiddleWare.js")
-
-
-// const registerUser = () => {};
+const protect = require("..//middleWare/authMiddleWare.js");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
 router.get("/getUser", protect, getUser);
+router.get("/loggedin", loginStatus);
 
 module.exports = router;
