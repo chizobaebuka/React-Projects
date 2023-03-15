77be5192be8d65269d87025f4c1d07a4b09896e8
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-15 12:01:45 +0100

    modified

--- sortly/backend/middleWare/authMiddleWare.js
+++ sortly/backend/middleWare/authMiddleWare.js
@@ -9,13 +9,15 @@ const protect = asyncHandler(async (req, res, next) => {
             res.status(401)
             throw new Error ("Not authorized, please login")
         }
+
         // verify token
         const verified = jwt.verify(token, process.env.JWT_SECRET);
+        
         // Get user id from token 
-        user = await User.findById(verified.id).select("-password");
+        const user = await User.findById(verified.id).select("-password");
         if(!user){
-            res.status(401)
-            throw new Error ("User not found")
+            res.status(401);
+            throw new Error ("User not found");
         }
         req.user = user
         next()
